7b39259e798c7b720c141998d1312bc7ebc2777c
shoei <shoei@github.com>
2025-06-16 10:32:50 +0900

    Playe処理追加

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
+    // Monster monster = new Monster();
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
